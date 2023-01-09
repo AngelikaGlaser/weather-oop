6a049bef819ededd728ae08a95d0078eb3ed9a5c
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-01-09 21:27:49 +0200

    Get weather data from API.

---
+++ classes/Weather.js
@@ -0,0 +1,13 @@
+class Weather{
+    constructor(city) {
+        this.city = city
+        this.key = '9f188c8987e25d0ea648592653f715c3'
+    }
+
+    async getWeather() {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
+        const responseData = await response.json()
+        return responseData
+    }
+}
+
